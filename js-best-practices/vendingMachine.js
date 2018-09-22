8f0efe3536b2d8f18606fca479eb793226364d9c
Lourdes Roashan <root.roashan@gmail.com>
2018-09-22 17:05:08 -0300

    JSP: EX6 - I cannot name variables

--- js-best-practices/vendingMachine.js
+++ js-best-practices/vendingMachine.js
@@ -19,6 +19,47 @@ module.exports = {
     return this.convertToChange(currentBalance);
   },
 
+  convertToChange: function (currentBalance) {
+    var changeArr = [];
+    r = parseInt(parseInt(currentBalance) / parseInt(25));
+    dr = currentBalance % 25;
+    if (r != 0) {
+
+      for (let i = 0; i < r; i++) {
+        changeArr.push("q");
+      }
+    }
+    if (dr >= 5) {
+      ndr = parseInt(parseInt(dr) / parseInt(10));
+      ndrx = dr % 10;
+      for (let i = 0; i < ndr; i++) {
+        changeArr.push("d");
+      }
+      if (ndrx >= 5) {
+        nndr = parseInt(parseInt(ndrx) / parseInt(5));
+        for (let i = 0; i < nndr; i++) {
+          changeArr.push("n");
+        }
+        nndrx = ndrx % 5;
+        for (let i = 0; i < nndrx; i++) {
+          changeArr.push("p");
+        }
+      }
+      else {
+        for (let i = 0; i < ndrx; i++) {
+          changeArr.push("p");
+        }
+      }
+    }
+    else {
+      for (let i = 0; i < dr; i++) {
+        changeArr.push("p");
+      }
+    }
+
+    return changeArr;
+  },
+
   isValidAmount: function (amount) {
     if (amount === null) {
       return false;
