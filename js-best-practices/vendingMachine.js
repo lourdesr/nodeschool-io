596c5278f93d108ea0de30d754f460ae829daf43
Lourdes Roashan <root.roashan@gmail.com>
2018-09-22 15:36:11 -0300

    JSP: EX2 - Complete

--- js-best-practices/vendingMachine.js
+++ js-best-practices/vendingMachine.js
@@ -3,7 +3,6 @@ var balanceManager = require('./balanceManager');
 var changeHandler = require('./changeHandler');
 var productInventory = require('./productInventory');
 
-var balance = 0;
 
 var products = [
   {
@@ -14,36 +13,15 @@ var products = [
 ];
 
 module.exports = {
-  canAfford: function(amount){
-    if(!this.isValidAmount(amount)){
-      errorMessage = "Invalid Input";
-    }
-    if(errorMessage){
-      throw new Error(errorMessage);
-    }
-    return amount <= balance;
-  },
 
-  decreaseBalance: function(amount){
-    // This method decreases the balance of the vending machine. If the balance amount is not 
-    // enough to cover the purchase, the method throws an error. 
-    var errorMessage;
-    if(!this.canAfford(amount)){
-        errorMessage = 'Insufficient balance';
-    }
-    if(errorMessage){
-        throw new Error(errorMessage);
-    }
-    balance -= amount;
-  },
 
-  getAmount: function(coinType) {
+  getAmount: function (coinType) {
     // COINS:
     // [p]enny
     // [n]ickel
     // [d]ime
     // [q]uarter
-    switch(coinType){
+    switch (coinType) {
       case 'p': return 1;
       case 'n': return 5;
       case 'd': return 10;
@@ -52,45 +30,37 @@ module.exports = {
     }
   },
 
-  getBalance: function(){ 
-    return balance;
-  },
-
-  getProducts: function() { 
+  getProducts: function () {
     return products;
   },
 
-  getProduct: function(productId) {
-    var product = products.find(function(p) { return p.id === productId; });
+  getProduct: function (productId) {
+    var product = products.find(function (p) { return p.id === productId; });
     return product;
   },
-  
-  increaseBalance: function(amount){
-    balance += amount;
-  },
 
-  insertCoin: function(coinType){
+  insertCoin: function (coinType) {
     var value = this.getAmount(coinType);
-    this.increaseBalance(value);
+    balanceManager.increaseBalance(value);
   },
 
-  isValidAmount: function(amount){
-    if(amount === null){
+  isValidAmount: function (amount) {
+    if (amount === null) {
       return false;
     } else {
       return true;
     }
   },
 
-  releaseChange: function(){
+  releaseChange: function () {
     var currentBalance = this.getBalance();
     this.decreaseBalance(currentBalance);
     return this.convertToChange(currentBalance);
   },
 
-  vendProduct: function(productId){
+  vendProduct: function (productId) {
     var product = this.getProduct(productId);
-    this.decreaseBalance(product.price);
+    balanceManager.decreaseBalance(product.price);
     return product;
   }
 
